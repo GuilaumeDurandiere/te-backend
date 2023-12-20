ed1d2a163a96cfb0c861f75ba97a69cc72770c94
Stéphane Fonte <stephane.fonte@ouidou.fr>
2023-12-20 15:35:36 +0100

    add new method + logger

---
+++ PortailTE44.Common/Dtos/SousEtapes/SousEtapeUpdatePayloadDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortailTE44.Common.Dtos.SousEtapes
+{
+    public class SousEtapeUpdatePayloadDto : SousEtapeBaseDto
+    {
+        public int Id { get; set; }
+    }
+}
