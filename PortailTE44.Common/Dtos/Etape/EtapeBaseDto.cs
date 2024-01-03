300d1c78ef9aa227705367f807742eddc5815191
Stéphane Fonte <stephane.fonte@gmail.com>
2024-01-03 09:45:03 +0100

    add list sous etape

--- PortailTE44.Common/Dtos/Etape/EtapeBaseDto.cs
+++ PortailTE44.Common/Dtos/Etape/EtapeBaseDto.cs
@@ -1,9 +1,12 @@
-﻿namespace PortailTE44.Common.Dtos.Etape
+﻿using PortailTE44.Common.Dtos.SousEtapes;
+
+namespace PortailTE44.Common.Dtos.Etape
 {
     public class EtapeBaseDto
     {
         public string Libelle { get; set; } = default!;
         public string? Description { get; set; }
         public string Statut { get; set; } = default!;
+        public IEnumerable<SousEtapeResponseDto> SousEtapes { get; set; }
     }
 }
