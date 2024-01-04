04f17191f0fcdf3b6fe28e3f96d216bcadccac5e
Stéphane Fonte <stephane.fonte@gmail.com>
2024-01-04 09:48:29 +0100

    Add Icone column

---
+++ PortailTE44.DAL/Migrations/M20240103_170000_add_columnIcone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentMigrator;
+
+namespace PortailTE44.DAL.Migrations
+{
+	[Migration(20240103_170000)]
+	public class M20240103_170000_add_columnIcone : Migration
+	{
+		public M20240103_170000_add_columnIcone()
+		{
+		}
+
+        public override void Down()
+        {
+            Delete.Column("Icone").FromTable("Theme");
+          
+        }
+
+        public override void Up()
+        {
+            Alter.Table("Theme")
+                .AddColumn("Icone").AsString().NotNullable();
+        }
+    }
+}
+
