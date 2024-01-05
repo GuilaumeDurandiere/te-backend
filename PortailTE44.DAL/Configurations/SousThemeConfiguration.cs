08b461eb95f72ddf9a2d3176db37785663cd3acc
Stéphane Fonte <stephane.fonte@gmail.com>
2024-01-05 14:38:45 +0100

    correct sous etape

---
+++ PortailTE44.DAL/Configurations/SousThemeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PortailTE44.DAL.Entities;
+
+namespace PortailTE44.DAL.Configurations
+{
+	internal class SousThemeConfiguration : IEntityTypeConfiguration<SousTheme>
+    {
+        public void Configure(EntityTypeBuilder<SousTheme> entity)
+        {
+            entity.ToTable("SousTheme");
+            entity.HasKey(t => t.Id);
+            entity.Property(t => t.Id)
+                  .ValueGeneratedOnAdd();
+            entity.HasOne(st => st.Theme)
+                  .WithMany(t => t.SousThemes)
+                  .HasForeignKey(e => e.ThemeId);
+        }
+    }
+}
+
---
+++ PortailTE44.DAL/Repositories/SousThemeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PortailTE44.DAL.Entities;
+using PortailTE44.DAL.Repositories.Interfaces;
+
+namespace PortailTE44.DAL.Repositories
+{
+	public class SousThemeRepository : GenericRepository<SousTheme>, ISousThemeRepository
+    {
+		public SousThemeRepository(PortailTE44Context context) : base(context)
+		{
+		}
+	}
+}
+
