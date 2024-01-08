d9bebc54d3aec03bac3dff31f752aa0e6a700c4f
Stéphane Fonte <stephane.fonte@gmail.com>
2024-01-08 14:15:45 +0100

    correct name error

--- PortailTE44.Common/Dtos/Theme/ThemeCreatePayloadDto.cs
+++ PortailTE44.Common/Dtos/Theme/ThemeCreatePayloadDto.cs
@@ -4,7 +4,7 @@ namespace PortailTE44.Common.Dtos.Theme
 {
 	public class ThemeCreatePayloadDto : ThemeBaseDto
 	{
-		public IEnumerable<SousThemeCreateOrUpdatePayloadDto> SousTheme { get; set; } = default!;
+		public IEnumerable<SousThemeCreateOrUpdatePayloadDto> SousThemes { get; set; } = default!;
 	}
 }
 
