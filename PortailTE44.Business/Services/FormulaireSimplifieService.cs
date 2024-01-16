f4c0decfad37bdabd84fcfa2d949317d1f9dcc03
Stéphane Fonte <stephane.fonte@gmail.com>
2024-01-16 16:01:04 +0100

    correct PR more config + display message

--- PortailTE44.Business/Services/FormulaireSimplifieService.cs
+++ PortailTE44.Business/Services/FormulaireSimplifieService.cs
@@ -3,6 +3,7 @@ using PortailTE44.Business.Services.Interfaces;
 using PortailTE44.Common.Dtos.Demande;
 using PortailTE44.Common.Enums;
 using PortailTE44.Common.Models;
+using PortailTE44.DAL.Configurations;
 using PortailTE44.DAL.Entities;
 using PortailTE44.DAL.Repositories.Interfaces;
 
@@ -12,38 +13,35 @@ namespace PortailTE44.Business.Services
 	{
         ISousThemeRepository _sousThemeRepository;
         IMailService _mailService;
+        IOptions<MailSettings> _mailSettings;
         FormulaireSimplifieResponsableTemplate formulaireSimplifieResponsableTemplate;
 
-		public FormulaireSimplifieService(ISousThemeRepository sousThemeRepository, IMailService mailService, IOptions<MailTemplates> options)
+		public FormulaireSimplifieService(ISousThemeRepository sousThemeRepository, IMailService mailService, IOptions<MailTemplates> templates, IOptions<MailSettings> settings)
         { 
             _sousThemeRepository = sousThemeRepository;
             _mailService = mailService;
-            formulaireSimplifieResponsableTemplate = options.Value.FormulaireSimplifieResponsable;
-
+            _mailSettings = settings;
+            formulaireSimplifieResponsableTemplate = templates.Value.FormulaireSimplifieResponsable;
         }
 
         public async Task<bool> FormulaireSimplifieResponsable(FormulaireSimplifieResponsablePayloadDto dto)
         {
             SousTheme? sousTheme = await _sousThemeRepository.GetByIdAsync(dto.SousThemeId);
+
             if(sousTheme is null)
-            {
                 throw new KeyNotFoundException($"Il n'existe aucun sous thème avec l'id {dto.SousThemeId}");
-            }
 
             if(sousTheme.RefTypeOffre.Id != (int)RefTypeOffreEnum.FORMULAIRE_SIMPLIFIE)
-            {
-                throw new Exception("Il ne s'agit pas d'un formulaire simplifiée");
-            }
+                throw new Exception("Il ne s'agit pas d'un formulaire simplifié");
             
-
-            return await _mailService.SendAsync(BuildMailData(dto, sousTheme), new CancellationToken());
+            return await _mailService.SendAsync(BuildFormulaireSimplifieResponsableMail(dto, sousTheme), new CancellationToken());
         }
 
-        private MailData BuildMailData(FormulaireSimplifieResponsablePayloadDto dto, SousTheme sousTheme)
+        private MailData BuildFormulaireSimplifieResponsableMail(FormulaireSimplifieResponsablePayloadDto dto, SousTheme sousTheme)
         {
             MailData data = new MailData();
             string template = File.ReadAllText(formulaireSimplifieResponsableTemplate.Path);
-            string mail = string.Format(template, "Te44", DateTime.Now, 1, sousTheme.Theme.Libelle, sousTheme.Libelle, "Origine", "Demandeur", dto.Telephone, "Signature");
+            string mail = string.Format(template, _mailSettings.Value.DisplayName, DateTime.Now, 1, sousTheme.Theme.Libelle, sousTheme.Libelle, "Origine", "Demandeur", dto.Telephone, dto.Message, "Signature");
             data.Subject = formulaireSimplifieResponsableTemplate.Subject;
             data.To = new List<string>() { sousTheme.MailReferent };
             data.Body = mail;
