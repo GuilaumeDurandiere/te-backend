097d7358a79ddb394c0c27989f347b4ab69a7f6d
Stéphane Fonte <stephane.fonte@gmail.com>
2024-01-19 18:02:46 +0100

    GDD-30-31 : create template formulaire adhesion

---
+++ PortailTE44.Business/Services/Interfaces/IFormulaireAdhesionService.cs
@@ -0,0 +1,10 @@
+﻿using PortailTE44.Common.Dtos.FormulaireAdhesion;
+
+namespace PortailTE44.Business.Services.Interfaces
+{
+	public interface IFormulaireAdhesionService
+	{
+        Task<bool> SendMailFormulaireAdhesion(FormulaireAdhesionPayloadDto dto);
+    }
+}
+
---
+++ PortailTE44.Exchange/Controllers/FormulaireAdhesionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortailTE44.Business.Services.Interfaces;
+using PortailTE44.Common.Dtos.FormulaireAdhesion;
+
+namespace PortailTE44.Exchange.Controllers
+{
+    [ApiController]
+    [Route("api/formulaireAdhesion")]
+    public class FormulaireAdhesionController
+	{
+        IFormulaireAdhesionService _formulaireAdhesionService;
+
+        public FormulaireAdhesionController(IFormulaireAdhesionService formulaireAdhesionService)
+        {
+            _formulaireAdhesionService = formulaireAdhesionService;
+        }
+
+        [HttpPost]
+        public async Task<bool> SendMailFormulaireAdhesion([FromBody] FormulaireAdhesionPayloadDto dto)
+        {
+            return await _formulaireAdhesionService.SendMailFormulaireAdhesion(dto);
+        }
+    }
+}
+
