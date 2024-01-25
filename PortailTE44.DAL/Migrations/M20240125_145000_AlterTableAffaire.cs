f3c184aff27aafd531aa28c055edad25e3f70201
nil.chemin@ouidou.fr <nil.chemin@ouidou.fr>
2024-01-25 15:02:59 +0100

    [NICH] - Scripts maj table affaire + sous theme

---
+++ PortailTE44.DAL/Migrations/M20240125_145000_AlterTableAffaire.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace PortailTE44.DAL.Migrations
+{
+    [Migration(20240125_145000)]
+    public class M20240125_145000_AlterTableAffaire : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Affaire").AddColumn("Ville").AsString().Nullable();
+            Alter.Table("Affaire").AddColumn("CodePostal").AsString().Nullable();
+        }
+
+        public override void Down()
+        {
+            if (Schema.Table("Affaire").Column("Ville").Exists())
+                Delete.Column("Ville").FromTable("Affaire");
+
+            if (Schema.Table("Affaire").Column("CodePostal").Exists())
+                Delete.Column("CodePostal").FromTable("Affaire");
+        }
+    }
+}
---
+++ PortailTE44.DAL/Migrations/M20240125_150000_AlterTableSousTheme.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace PortailTE44.DAL.Migrations
+{
+    [Migration(20240125_150000)]
+    public class M20240125_150000_AlterTableSousTheme : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("SousTheme").AddColumn("ServiceCompetenceId").AsInt32().Nullable().ForeignKey("ServiceCompetence", "Id");
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_SousTheme_ServiceCompetenceId_ServiceCompetence_Id").OnTable("SousTheme");
+            Delete.Column("ServiceCompetenceId").FromTable("SousTheme");
+        }
+    }
+}
